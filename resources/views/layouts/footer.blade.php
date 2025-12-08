aea5230e3b4410b1a47b91287e59bc37152c90e4
Janiya-99 <janithsamarasinghe1999@gmail.com>
2025-12-08 08:30:02 +0530

    Changed the logo

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="pc-footer">
+  <div class="footer-wrapper container-fluid">
+    <div class="row">
+      <div class="col-sm-6 my-1">
+        <script>document.write(new Date().getFullYear())</script> © Designed & Developed by <a href="https://asipiya.lk/" style="color:#000000"><img src="https://www.asipiya.lk/assets/logo-f600df78.png" alt="Asipiya" style="position: relative; bottom: 3px; left: 4px;" width="80px"></a>.
+      </div>
+      <div class="col-sm-6 ms-auto my-1">
+        <ul class="list-inline footer-link mb-0 justify-content-sm-end d-flex">
+          {{-- <li class="list-inline-item"><a href="/index">Home</a></li>
+          <li class="list-inline-item"><a href="https://pcoded.gitbook.io/light-able/" target="_blank">Documentation</a></li>
+          <li class="list-inline-item"><a href="https://phoenixcoded.support-hub.io/" target="_blank">Support</a></li> --}}
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
